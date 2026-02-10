2ed54a3523def8ef164ce406e01a96108379dc78
godfredboakye93-tech <godfredboakye93@gmail.com>
2026-02-10 20:18:18 +0100

    goed bezig

--- topics/fundamentals.starter.js
+++ topics/fundamentals.starter.js
@@ -5,26 +5,56 @@
 (__)  \____/\_)__)(____/\_/\_/\_)(_/(____)\_)__) (__)\_/\_/\____/(____/
  */
 
-// 1. link this script file to your page and open your developer console
+// ✅1. link this script file to your page and open your developer console
 
-// 2. create a variable that keeps track of your name (can be let, const or var)
+// ✅2. create a variable that keeps track of your name (can be let, const or var)
+    let name = "";
 
-// 3. create a variable that asks for your birthyear (use prompt() for that)
+// ✅3. create a variable that asks for your birthyear (use prompt() for that)
 // hint: prompt() returns a string, so you'll need to convert it to a number with parseInt()
+    let birthYear = prompt("what's your birthyear?", "2005");
+    console.log(birthYear);
 
-// 4. calculate your age (this will require grabbing the current year somehow)
+// ✅ 4. calculate your age (this will require grabbing the current year somehow)
 // hint: you can get the current year with new Date().getFullYear()
+let currentYear = new Date().getFullYear(); 
+let age = currentYear - birthYear;  
 
-// 5. log your age to the console
+
+
+//what je doet is je spreekt aan wat je nodig hebt zoals het huidge jaar en dan vergeet niet het in de console te laten tonen.
+
+
+
+// ✅ 5. log your age to the console
+    console.log(age);
 
 // 6. write a function named greet() that wishes you a nice day like `Hi there, [your name]! Have a nice day!`
 // hint: you can use string interpolation with backticks to insert your name into the string
 // hint: the function should take [your name] as an argument
 // use the function to print the message to the console
+    function greet(name) {
+      console.log(`Hi there, ${name}! Have a nice day!`);
+
+    } 
+
+greet("G-Fred");
+
+//wat je doet is een functie maken en in die functie roep je naam aan dan zet je de text in je console 
+// en maak je een referentie naar iets of iemand zoals greet ...
 
 // 7. write a function named calculateAge() that calculates your age and logs it to the console
 // use the function to print your age to the console
 // hint: the function should take your birthyear as an argument
+function calculateAge(birthYear) {
+    let currentYear = new Date().getFullYear(); 
+    let age = currentYear - birthYear; 
+    console.log(age);
+} 
+
+calculateAge(2005)
+
+//maak een functie en zet wat je wil aanspreken erin dan maak je variabelen en vergeet de console log niet.
 
 // 8. use prompt() to ask if you how you are feeling today ("good", "bad", "ok")
 // store the result in a variable
